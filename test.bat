ff1da2de53126af4c7a37bc8daea759d5b548876
Mikael Egevig <mikael@egevig.org>
2025-02-28 02:57:52 +0100

    Added explicit check for the existence of the RAMDISK environment variable.

--- test.bat
+++ test.bat
@@ -1,5 +1,17 @@
 @echo off
 setlocal
+
+if "%RAMDISK%" == "" goto ErrorRamdisk
+
 set project=KioskForge
 mypy --cache-dir %RAMDISK%\%project% --strict %project%.py
+
+goto Epilogue
+
+:ErrorRamdisk
+echo Error: RAMDISK environment variable not defined
+goto Epilogue
+
+:Epilogue
 endlocal
+
