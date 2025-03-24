905f139b51a088c6d02b66e91654f4ce1ab344c6
Mikael Egevig <mikael@egevig.org>
2025-03-24 05:28:43 +0100

    Added currently disabled options in preparation for shipping KioskForge as a Windows console program.

--- build.bat
+++ build.bat
@@ -1,2 +1,6 @@
 @echo off
+rem TODO: Figure out how to make the Linux scripts, and the 'kiosk' module, available to the KioskForge.exe program.
+rem https://stackoverflow.com/questions/11322538/including-a-directory-using-pyinstaller provides tips.
+rem --add-data="KioskSetup.py:." --add-data="KioskStart.py:."
 pyinstaller --console --noupx --onefile --distpath R:\KioskForge-SHIP --workpath R:\KioskForge-TEMP --upx-exclude python3.dll KioskForge.py
+
