1b1fe5e41e4fa9b2bd4c54ff190c4350f2aadd33
Mikael Egevig <mikael@egevig.org>
2025-03-24 02:13:16 +0100

    Updated to reflect the change that KioskForge.py now is split into three scripts.

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
 set -e
 
+if [ -z "$RAMDISK" ]; then
+	echo Error: RAMDISK environment variable not defined
+	exit 1
+fi
+
 project=KioskForge
-mypy --cache-dir $RAMDISK/$project --strict $project.py
+mypy --cache-dir $RAMDISK/$project --strict KioskForge.py KioskSetup.py KioskStart.py
+
