0e2b551f8a298a8ca54012e9f2694bd272610f2a
Mikael Egevig <me@vhm.dk>
2025-04-03 05:09:18 +0200

    Fixed incorrect 'print' statement (should be 'write').

--- toolbox/driver.py
+++ toolbox/driver.py
@@ -82,7 +82,7 @@ class KioskDriver(object):
 				else:
 					text = str(that)
 				logger.error("Fatal error: %s" % text)
-				logger.print(traceback.format_exc())
+				logger.write(traceback.format_exc())
 
 		# If not running from a console, wait for a keypress so that the user can read the output.
 		if platform.system() == "Windows" and not "PROMPT" in os.environ:
