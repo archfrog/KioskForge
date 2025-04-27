aecb38668d252e366e22a0c522f1edc62ad116c6
Mikael Egevig <me@vhm.dk>
2025-04-28 01:26:35 +0200

    Eliminated one 'using-constant-test' pylint warning.

--- build.py
+++ build.py
@@ -186,10 +186,9 @@ class KioskBuild(KioskDriver):
 		words += "--workpath"
 		words += workpath
 
-		# NOTE: The --specpath option also affects the default location of data files, something I think is pretty bizarre.
-		if False:
-			words += "--specpath"
-			words += distpath
+		# NOTE: The --specpath option also affects the default location of data files, something which I think is pretty bizarre.
+		# words += "--specpath"
+		# words += distpath
 
 		words += "--upx-exclude"
 		words += "python3.dll"
