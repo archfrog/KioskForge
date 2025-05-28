f5d36433393c6eb8b2ea0293a195c53e2e4f28cb
Mikael Egevig <me@vhm.dk>
2025-05-28 08:36:44 +0200

    Bumped version to v0.22.

--- toolbox/version.py
+++ toolbox/version.py
@@ -19,7 +19,7 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 PRODUCT = "KioskForge"
-VERSION = "0.21"
+VERSION = "0.22"
 COMPANY = "Vendsyssel Historiske Museum"
 CONTACT = "Mikael Egevig <me@vhm.dk>"
 WEBSITE = "https://kioskforge.org/"
