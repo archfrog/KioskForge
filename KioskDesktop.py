ca079e4167eaf1e9febb4881242281d617a6a717
Mikael Egevig <mikael@egevig.org>
2026-05-14 06:08:08 +0200

    Changed to support monitoring and restarting both X11 and web apps.

--- KioskDesktop.py
+++ KioskDesktop.py
@@ -29,6 +29,7 @@
 from typing import List
 
 import os
+import shlex
 import shutil
 import subprocess
 import sys
@@ -49,6 +50,143 @@ KIOSKFORGE_TO_XRANDR_ROTATIONS = {
 	'right' : 'right'
 }
 
+
+class Program:
+	"""An abstraction of a program that can be invoked and monitored by the Monitor class."""
+
+	def __init__(self, kiosk : Kiosk):
+		self._kiosk = kiosk
+
+	@property
+	def command(self) -> List[str]:
+		raise TypeError("Abstract method called")
+
+	@property
+	def description(self) -> str:
+		raise TypeError("Abstract method called")
+
+
+class ChromiumProgram(Program):
+	"""A concrete implemention of the Chromium browser."""
+
+	@property
+	def command(self) -> List[str]:
+		"""Builds the Chromium command-line."""
+		# Build the Chromium command line with a horde of options (I don't know which ones work and which don't...).
+		# NOTE: Chromium does not complain about any of the options listed below!
+		command  = TextBuilder()
+		command += shutil.which("chromium") or "chromium"
+		command += "--kiosk"
+		command += "--fast"
+		command += "--fast-start"
+		command += "--start-maximised"
+		command += "--noerrdialogs"
+		command += "--no-first-run"
+		command += "--enable-pinch"
+		command += "--touch-events=enabled"
+		command += "--overscroll-history-navigation=disabled"
+		command += "--disable-features=TouchpadOverscrollHistoryNavigation"
+		command += "--overscroll-history-navigation=0"
+		command += "--disable-restore-session-state"
+		command += "--disable-infobars"
+		command += "--disable-crashpad"
+
+		# Enable automatic autoplay of videos, if requested by the user.
+		if self._kiosk.chromium_autoplay.data:
+			command += "--autoplay-policy=no-user-gesture-required"
+
+		# Use the /tmp folder for the disk cache, if kiosk wear reduction has been enabled.
+		if self._kiosk.wear_reduction.data:
+			command += "--disk-cache-dir=/tmp/Chromium"
+
+		# Append the URL of the website, local or online, to be browsed.
+		command += self._kiosk.command.data
+
+		return command.list
+
+	@property
+	def description(self) -> str:
+		return "Chromium web browser"
+
+
+class CustomProgram(Program):
+	"""A concrete implementation of a user-specified program (command=)."""
+
+	@property
+	def command(self) -> List[str]:
+		"""Builds the user-specified command's command-line."""
+		strings = shlex.split(self._kiosk.command.data)
+		return [os.path.normpath(os.path.join("/home/kiosk/", strings[0]))] + strings[1:]
+
+	@property
+	def description(self) -> str:
+		return "user-defined program"
+
+
+class Monitor:
+	"""A simple monitor that terminates the program if it times out and/or restarts the invoked program (also on crashes)."""
+
+	# Returns the total number of seconds (with no fraction) of idle time since the X server was last busy.
+	@staticmethod
+	def x_idle_time() -> int:
+		result = invoke_text("xprintidle")
+		if result.status != 0:
+			raise KioskError("Unable to get idle time from X11 window manager")
+		return int(result.output) // 1000
+
+	@staticmethod
+	def invoke(logger : Logger, program : Program, timeout : int) -> None:
+		# Timeout value is either 0 (disabled) or other (number of seconds).
+
+		signal = Signal("KioskDesktop-shutdown", "kiosk")
+		try:
+			# Launch the program forever (until this script is asked to shut down), restarting it if terminated or crashed.
+			while not signal.exists:
+				# Launch the program in the background as a detached process.
+				try:
+					# pylint: disable-next=consider-using-with
+					process = subprocess.Popen(program.command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+				except subprocess.CalledProcessError as that:
+					raise KioskError(program.description + " failed to start:" + that.output.decode('utf-8')) from that
+				except subprocess.SubprocessError as that:
+					raise KioskError("Unable to start " + program.description) from that
+				except OSError as that:
+					raise KioskError(that.strerror or "Unknown OS error") from that
+
+				# Let the program start completely before we begin to check if it has been idle for too long.
+				time.sleep(15)
+
+				# Loop forever (until asked to shut down), launching the program and terminating it if it is idle for too long.
+				while not signal.exists:
+					# Wait one second between checking if the program should be restarted.
+					time.sleep(1)
+
+					# If the program has exited (crashed), exit to outer loop to restart it.
+					if "process" in locals() and process.poll():
+						logger.error("Restarting " + program.description + " after crash.")
+						break
+
+					# If the app has been idle for more than N seconds, terminate the program and exit to outer loop to restart it.
+					if timeout and Monitor.x_idle_time() >= timeout:
+						process.terminate()
+						del process
+
+						# Reset X11's idle timer to ensure that inaccuracies do not accumulate over time.
+						invoke_text_safe("xset s reset")
+
+						break
+		finally:
+			# Terminate the program if it is still running.
+			if "process" in locals():
+				if not process.poll():		# pyrefly: ignore[unbound-name]
+					process.terminate()		# pyrefly: ignore[unbound-name]
+				del process					# pyrefly: ignore[unbound-name]
+
+			# Remove the signal once we've performed the requested task.
+			signal.remove()
+			del signal
+
+
 class KioskDesktop(KioskDriver):
 	"""Defines the KioskDesktop class, which starts the X11 desktop, and any app, monitors it, and restarts it if necessary.
 
@@ -58,21 +196,13 @@ class KioskDesktop(KioskDriver):
 	def __init__(self) -> None:
 		KioskDriver.__init__(self)
 
-	# Returns the total number of seconds (with a fraction) of idle time since the X server was last busy.
-	@staticmethod
-	def x_idle_time() -> float:
-		result = invoke_text("xprintidle")
-		if result.status != 0:
-			raise KioskError("Unable to get idle time from X Windows")
-		return int(result.output) / 1000
-
 	def _main(self, logger : Logger, origin : str, arguments : List[str]) -> None:
 		# Check that we're running on Linux.
-		if sys.platform != "linux":
+		if sys.platform != "linux":					# pylint: disable=duplicate-code
 			raise KioskError("This script can only be run on a Linux kiosk machine")
 
 		# Check that we don't have root privileges.
-		# pylint: disable-next=E1101
+		# pylint: disable-next=no-member
 		if os.geteuid() == 0:		# pyrefly: ignore[missing-attribute]
 			raise KioskError("You may not be root when running this script")
 
@@ -104,94 +234,14 @@ class KioskDesktop(KioskDriver):
 
 		# Only launch Chromium if a 'web' type kiosk: the 'x11' type uses a custom application supplied by the user.
 		if kiosk.type.data == "web":
-			signal = Signal("KioskDesktop-shutdown", "kiosk")
-			try:
-				# Build the Chromium command line with a horde of options (I don't know which ones work and which don't...).
-				# NOTE: Chromium does not complain about any of the options listed below!
-				command  = TextBuilder()
-				command += shutil.which("chromium") or "chromium"
-				command += "--kiosk"
-				command += "--fast"
-				command += "--fast-start"
-				command += "--start-maximised"
-				command += "--noerrdialogs"
-				command += "--no-first-run"
-				command += "--enable-pinch"
-				command += "--touch-events=enabled"
-				command += "--overscroll-history-navigation=disabled"
-				command += "--disable-features=TouchpadOverscrollHistoryNavigation"
-				command += "--overscroll-history-navigation=0"
-				command += "--disable-restore-session-state"
-				command += "--disable-infobars"
-				command += "--disable-crashpad"
-				if kiosk.chromium_autoplay.data:
-					command += "--autoplay-policy=no-user-gesture-required"
-				if kiosk.wear_reduction.data:
-					command += "--disk-cache-dir=/tmp/Chromium"
-				command += kiosk.command.data
-				cmdlist  = command.list
-				del command
-
-				# Forever launch Chromium, possibly terminate it, and restart it again if terminated or crashed.
-				while not signal.exists:
-					# Launch Chromium in the background as a detached process.
-					try:
-						# pylint: disable-next=consider-using-with
-						process = subprocess.Popen(cmdlist, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-					except subprocess.SubprocessError as that:
-						raise KioskError("Unable to launch Chromium") from that
-					except OSError as that:
-						raise KioskError(that.strerror or "Unknown OS error") from that
-
-					# Let Chromium start before we begin to check if it has been idle for too long.
-					time.sleep(15)
-
-					# Loop forever (until asked to shut down), launching Chromium and terminating it if it becomes idle for N seconds.
-					while not signal.exists:
-						# Wait one second between checking if Chromium should be restarted.
-						time.sleep(1)
-
-						# If Chromium has exited (crashed), exit to outer loop to restart it.
-						if "process" in locals() and process.poll():
-							logger.error("Restarting Chromium after crash.")
-							break
-
-						# If X has been idle for more than N seconds, terminate Chromium and exit to outer loop to restart it.
-						if timeout and self.x_idle_time() >= timeout:
-							process.terminate()
-							del process
-
-							# Reset X's idle timer to ensure that inaccuracies do not accumulate over time.
-							invoke_text_safe("xset s reset")
-
-							break
-			finally:
-				# Terminate Chromium if it is still running.
-				if "process" in locals():
-					if not process.poll():		# pyrefly: ignore[unbound-name]
-						process.terminate()		# pyrefly: ignore[unbound-name]
-					del process					# pyrefly: ignore[unbound-name]
-
-				# Remove the signal once we've performed the requested task.
-				signal.remove()
-				del signal
+			program = ChromiumProgram(kiosk)
 		elif kiosk.type.data == "x11":
-			# Launch the app in the foreground.
-			try:
-				subprocess.run(kiosk.command.data, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True)
-			except subprocess.CalledProcessError as that:
-				raise KioskError("User application failure: " + that.output.decode('utf-8')) from that
-			except subprocess.SubprocessError as that:
-				raise KioskError("Unable to launch user application") from that
-			except OSError as that:
-				raise KioskError(that.strerror or "Unknown OS error") from that
-			finally:
-				if "process" in locals():
-					if not process.poll():		# pyrefly: ignore[unbound-name]
-						process.terminate()		# pyrefly: ignore[unbound-name]
-					del process					# pyrefly: ignore[unbound-name]
+			program = CustomProgram(kiosk)
 		else:
 			raise KioskError("Kiosk type not supported by KioskDesktop.py: " + kiosk.type.data)
+
+		# Start the program and keep restarting it on crashes until the signal 'KioskDesktop-shutdown' is signalled.
+		Monitor().invoke(logger, program, timeout)
 
 
 if __name__ == "__main__":
