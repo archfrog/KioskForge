c6db543f67ca57ba51aab259a8f784682af8ea5e
Mikael Egevig <mikael@egevig.org>
2026-06-02 17:18:00 +0200

    Bumped version to v1.05 as v1.04 had some serious flaws so I am making another release today.

--- kiosklib/version.py
+++ kiosklib/version.py
@@ -24,7 +24,7 @@
 #**********************************************************************************************************************************
 
 PRODUCT = "KioskForge"
-VERSION = "1.04"
+VERSION = "1.05"
 COMPANY = "The KioskForge Team"
 CONTACT = "contact@kioskforge.org"
 WEBSITE = "https://kioskforge.org/"
