5fe9fc4b3cefec7db0f0cd14f6f04d918f53db1a
Mikael Egevig <mikael@egevig.org>
2026-09-13 00:50:06 +0200

    Bumped to v1.12.

--- kiosklib/version.py
+++ kiosklib/version.py
@@ -24,7 +24,7 @@
 #**********************************************************************************************************************************
 
 PRODUCT = "KioskForge"
-VERSION = "1.11"
+VERSION = "1.12"
 COMPANY = "The KioskForge Team"
 CONTACT = "contact@kioskforge.org"
 WEBSITE = "https://kioskforge.org/"
